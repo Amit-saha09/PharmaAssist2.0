a9cb8188b65eee4fbe68212f69b4d8bc43a86cc1
AbirShimanto <shimantohassan@yahoo.com>
2021-06-30 11:34:34 +0600

    First Commit User

--- PharmaAssist2.0/PharmaAssist2.0/App_Start/BundleConfig.cs
+++ PharmaAssist2.0/PharmaAssist2.0/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace PharmaAssist2._0
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
         }
     }
-}
+    }
+
