ca1fa26c440030911778c6007e5a1b00eea828a6
Zishad Hossain Limon <zishadlimon@gmail.com>
2021-07-02 20:08:27 +0600

    Admin

--- PharmaAssist2.0/PharmaAssist2.0/Controllers/AdminController.cs
+++ PharmaAssist2.0/PharmaAssist2.0/Controllers/AdminController.cs
@@ -1,24 +1,40 @@
-﻿using System;
+﻿using PharmaAssist2._0.Repository;
+using PharmaAssist2._0.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace PharmaAssist2._0.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult AdminsManagement()
         {
-            return View();
+            AdminRepository ar = new AdminRepository();
+            var admins = ar.GetAll();
+            return View(admins);
         }
 
+        [HttpGet]
+        public ActionResult GetAdminsByName(string id)
+        {
+            AdminRepository ar = new AdminRepository();
+            var admins = ar.GetAdminsByName(id);
+            return this.Json(admins, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
         public ActionResult ManagersManagement()
         {
             return View();
---
+++ PharmaAssist2.0/PharmaAssist2.0/Repository/AdminRepository.cs
@@ -0,0 +1,16 @@
+﻿using PharmaAssist2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaAssist2._0.Repository
+{
+    public class AdminRepository : Repository<Admin>
+    {
+        public List<Admin> GetAdminsByName(string name)
+        {
+            return this.contex.Admins.Where(x => x.Name.Contains(name)).ToList();
+        }
+    }
+}
