dad7566028229bd67498e470a21708ca2a31bcff
Amit-saha09 <amitamitsaha09@gmail.com>
2021-07-02 20:06:49 +0600

    admin

--- PharmaAssist2.0/PharmaAssist2.0/Models/Admin.cs
+++ PharmaAssist2.0/PharmaAssist2.0/Models/Admin.cs
@@ -14,11 +14,7 @@ namespace PharmaAssist2._0.Models
         public string Name { get; set; }
         [Required]
         public int? LoginId { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Required]
+        
         public string Phone { get; set; }
         [Required]
         public string Address { get; set; }
