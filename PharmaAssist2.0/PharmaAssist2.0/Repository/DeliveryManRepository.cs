cfd5f1bb2d7bf6ecd92db19dcaf42dff98c718ae
Zishad Hossain Limon <zishadlimon@gmail.com>
2021-07-05 14:28:18 +0600

    /

--- PharmaAssist2.0/PharmaAssist2.0/Repository/DeliveryManRepository.cs
+++ PharmaAssist2.0/PharmaAssist2.0/Repository/DeliveryManRepository.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace PharmaAssist2._0.Repository
 {
-    public class DeliveryManRepository: Repository<DeliveryMen>
+    public class DeliveryManRepository: Repository<DeliveryMan>
     {
     }
 }
